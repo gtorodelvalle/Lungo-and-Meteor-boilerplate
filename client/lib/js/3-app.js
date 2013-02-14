fc385469973f82dbcb58f0a0e9d5e0c6a7fac624
Germán Toro del Valle <gtorodelvalle@gmail.com>
2013-02-14 20:57:12 +0100

    First upload of files.

---
+++ client/lib/js/3-app.js
@@ -0,0 +1,17 @@
+Meteor.startup(function() { // Uncomment for Meteor.
+    Lungo.init({
+        data: 'Lungo Gallery app',
+        version: '2.0',
+        resources: []
+    });
+}); // Uncomment for Meteor.
+
+$$('#latest-apps li').swipeLeft(function (ev) {
+    $$(ev.target).closest('li').addClass("light");
+    console.log("Read!");
+});
+
+$$('#latest-apps li').swipeRight(function (ev) {
+    $$(ev.target).closest('li').removeClass("light");
+    console.log("Unread!");
+});
